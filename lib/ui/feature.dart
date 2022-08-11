e5e02196469851aed0f58ae1929441245f51a261
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2022-08-11 18:42:38 +0100

    Add the feature.dart items

---
+++ lib/ui/feature.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class FeaturesPack extends StatefulWidget {
+  FeaturesPack({required this.albumId, Key? key}) : super(key: key);
+  int albumId;
+
+  @override
+  State<FeaturesPack> createState() => _FeaturesPackState();
+}
+
+class _FeaturesPackState extends State<FeaturesPack> {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text('Album Title ${widget.albumId}'),
+        ListView.builder(
+            itemCount: 4,
+            itemBuilder: (BuildContext context, int index) {
+              return SizedBox(
+                height: 150,
+                width: 150,
+                child: Image.network(''),
+              );
+            }
+        )
+      ],
+    );
+  }
+}
