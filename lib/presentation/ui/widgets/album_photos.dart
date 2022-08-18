cc555cd9c907fe1e25381ff093e2bb73db6c81e1
Ayanfe Afolabi <afolabi.ayanfe@decagon.dev>
2022-08-18 21:55:52 +0100

    Added internet permission

--- lib/presentation/ui/widgets/album_photos.dart
+++ lib/presentation/ui/widgets/album_photos.dart
@@ -1,4 +1,6 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:cypressoft_practical_task/data/model/photoAlbumModel.dart';
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import '../../../data/repository/album.repository.dart';
 
@@ -23,35 +25,27 @@ class _AlbumPhotoListsState extends State<AlbumPhotoLists> {
           return SizedBox(
             height: 175,
             width: double.maxFinite,
-            child: ListView.builder(
-                // itemCount: snapshot.data?.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (BuildContext context, int index) {
+            child: CarouselSlider.builder(
+                itemCount: snapshot.data?.length,
+                itemBuilder: (BuildContext context, int index, int pageViewIndex){
                   var currentAlbum = snapshot.data?[index];
-                  return Padding(
-                    padding: const EdgeInsets.only(left: 8, right: 8),
-                    child: SizedBox(
-                      height: 150,
-                      width: 150,
-                      child: Image.network(
-                        currentAlbum?.thumbnailUrl ??
-                            "https://via.placeholder.com/150/92c952",
-                        loadingBuilder: (BuildContext context, Widget child,
-                            ImageChunkEvent? loadingProgress) {
-                          if (loadingProgress == null) return child;
-                          return Center(
-                            child: CircularProgressIndicator(
-                              value: loadingProgress.expectedTotalBytes != null
-                                  ? loadingProgress.cumulativeBytesLoaded /
-                                      loadingProgress.expectedTotalBytes!
-                                  : null,
-                            ),
-                          );
-                        },
-                      ),
+                  return SizedBox(
+                    height: 150,
+                    width: 150,
+                    child: ExtendedImage.network(
+                      currentAlbum?.thumbnailUrl ?? "",
+                      cache: true,
                     ),
                   );
-                }),
+                },
+                options: CarouselOptions(
+                  enableInfiniteScroll: true,
+                  scrollDirection: Axis.horizontal,
+                  enlargeCenterPage: false,
+                  viewportFraction: 0.45,
+                  height: double.maxFinite,
+                )
+            ),
           );
         }
         if (snapshot.hasError) {
